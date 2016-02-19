0121f7d0012ec79092fe8284fb807131c77053cc
nabettu <tatsuaki.w@gmail.com>
2016-02-19 16:39:20 +0900

    変数でなくobjectを

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 $(function() {
+
   $.ajax({
     url: "http://nabettu.php.xdomain.jp/instaScraper/?u=niwan5",
     type: "GET",
     success: function(data) {
-      $("body").append(data);
-      var photos = window._sharedData.entry_data.ProfilePage[0].user.media.nodes;
+      var scriptDom = $("<script></script>");
+      scriptDom.html("window._data1 = " + data);
+      $("body").append(scriptDom);
+      var photos = window._data1.entry_data.ProfilePage[0].user.media.nodes;
       $.each(photos,function(index,photo){
         var $imgDom = $("<img />");
         $imgDom.attr("src",photo.thumbnail_src);
