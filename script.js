15522311b87c6bfcf14aa4c01e2c0bba70be9d8b
nabettu <tatsuaki.w@gmail.com>
2016-02-19 17:28:18 +0900

    id指定

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 $(function() {
 
-  $.ajax({
-    url: "http://nabettu.php.xdomain.jp/instaScraper/?u=niwan5",
-    type: "GET",
-    success: function(data) {
+  var user = [getQueryString().u0, getQueryString().u1];
+  for (var i = 0; i < user.length; i++) {
+    $.ajax({
+      url: "http://nabettu.php.xdomain.jp/instaScraper/?u=" + user[i],
+      context: {
+        username: user[i],
+        num: i
+      }
+    }).done(function(data) {
       var scriptDom = $("<script></script>");
-      scriptDom.html("window._data1 = " + data);
+      var ctx = this;
+      scriptDom.html("window._u_" + ctx.username + " = " + data);
       $("body").append(scriptDom);
-      var photos = window._data1.entry_data.ProfilePage[0].user.media.nodes;
-      $.each(photos,function(index,photo){
+      var photos = window["_u_" + ctx.username].entry_data.ProfilePage[0].user.media.nodes;
+      $.each(photos, function(index, photo) {
         var $imgDom = $("<img />");
-        $imgDom.attr("src",photo.thumbnail_src);
-        $("#photo").append($imgDom);
-      })
-    }
-  });
+        $imgDom.addClass("u" + ctx.num)
+          .attr({
+            "src": photo.thumbnail_src,
+            "id": "u" + ctx.num + "_" + index
+          });
+        $("#u" + ctx.num).append($imgDom);
+      });
+    });
+  }
 })
+
+
+function getQueryString() {
+  var result = {};
+  if (1 < window.location.search.length) {
+    var query = window.location.search.substring(1);
+    var parameters = query.split('&');
+    for (var i = 0; i < parameters.length; i++) {
+      var element = parameters[i].split('=');
+      var paramName = decodeURIComponent(element[0]);
+      var paramValue = decodeURIComponent(element[1]);
+      result[paramName] = paramValue;
+    }
+  }
+  return result;
+}
